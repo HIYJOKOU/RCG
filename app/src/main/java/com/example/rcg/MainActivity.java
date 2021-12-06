27d6c3e67e9e5e3393976fb6847ad428ba026a87
hiyjokou <hiyjokou@gmail.com>
2021-12-06 14:24:45 +0100

    0.15V
    #Desc
    -Font Texts
    -Alpha in Texts
    -Background Color
    -Style,Colors.xml little changes

--- app/src/main/java/com/example/rcg/MainActivity.java
+++ app/src/main/java/com/example/rcg/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.rcg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 }
