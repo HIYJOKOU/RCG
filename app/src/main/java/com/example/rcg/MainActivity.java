311b6ee19661d64468358126a2094bbbd7bf8c40
hiyjokou <hiyjokou@gmail.com>
2021-12-07 13:44:51 +0100

    Alpha Version 1.35.870
    #Update Note:
    
    -Created a scanner and button working

--- app/src/main/java/com/example/rcg/MainActivity.java
+++ app/src/main/java/com/example/rcg/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.rcg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, scanner.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/rcg/scanner.java
+++ app/src/main/java/com/example/rcg/scanner.java
@@ -3,12 +3,17 @@ package com.example.rcg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Objects;
 
 public class scanner extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_scanner);
     }
 }
