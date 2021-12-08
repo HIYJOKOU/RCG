2fb792b9a387eff0ace7f90937b627d4f81f07c8
hiyjokou <hiyjokou@gmail.com>
2021-12-08 14:39:16 +0100

    Alpha Version 1.75.00
    #Update Note:
    -Napis Good Morning/Good Evening w zaleznosci od godziny
    
    -Skaner skanuje i podaje poprawne numery kodów

--- app/src/main/java/com/example/rcg/MainActivity.java
+++ app/src/main/java/com/example/rcg/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.rcg;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
+import java.util.Calendar;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
+        changeTimeText();
+
         Button btn = (Button)findViewById(R.id.button);
 
         btn.setOnClickListener(new View.OnClickListener() {
@@ -26,5 +33,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+
+    public void changeTimeText(){
+        Calendar cal = Calendar. getInstance();
+        int Time = cal. get(Calendar. HOUR_OF_DAY);
+
+        TextView PrimaryText = (TextView) findViewById(R.id.PrimaryText);
+        if (Time >= 18) {
+            Log.e("changeTimeText","Changed to Evening");
+            PrimaryText.setText(getResources().getString(R.string.front_text_1_Evening));
+        } else {
+            Log.e("changeTimeText","Changed to Morning");
+            PrimaryText.setText(getResources().getString(R.string.front_text_1_Morning));
+        }
     }
 }
