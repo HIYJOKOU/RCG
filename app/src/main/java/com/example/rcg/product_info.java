5622316bc5663203f38dacdb4845ee470ad67424
HIYJOKOU <hiyjokou@gmail.com>
2021-12-09 02:30:41 +0100

    Alpha Version 2.85.700
    *Patch Note*
    
    ~Changes in activity_product_info.xml
    ~
    ~

--- app/src/main/java/com/example/rcg/product_info.java
+++ app/src/main/java/com/example/rcg/product_info.java
@@ -2,9 +2,20 @@ package com.example.rcg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
+
+import java.io.InputStream;
 import java.util.Objects;
 
 public class product_info extends AppCompatActivity {
@@ -15,5 +26,29 @@ public class product_info extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_product_info);
+
+
+        new DownloadImageFromInternet((ImageView) findViewById(R.id.imageView2)).execute("https://icon-library.com/images/loading-icon-png/loading-icon-png-28.jpg");
+    }
+    private class DownloadImageFromInternet extends AsyncTask<String, Void, Bitmap> {
+        ImageView imageView;
+        public DownloadImageFromInternet(ImageView imageView) {
+            this.imageView=imageView;
+        }
+        protected Bitmap doInBackground(String... urls) {
+            String imageURL=urls[0];
+            Bitmap bimage=null;
+            try {
+                InputStream in=new java.net.URL(imageURL).openStream();
+                bimage= BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error Message", e.getMessage());
+                e.printStackTrace();
+            }
+            return bimage;
+        }
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
     }
 }
